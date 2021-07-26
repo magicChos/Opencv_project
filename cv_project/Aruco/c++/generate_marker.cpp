7ea61571ad2f54b536f8ea0943dacef0cba9136c
hanshuo <hanshuo@seedland.com>
2021-07-26 15:10:12 +0800

    add update

--- cv_project/Aruco/c++/generate_marker.cpp
+++ cv_project/Aruco/c++/generate_marker.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/aruco.hpp>
+#include <opencv2/aruco/charuco.hpp>
 #include <iostream>
 #include <vector>
 #include <Eigen/Eigen>
@@ -12,72 +13,11 @@
 #include "poseestamation.h"
 #include <memory>
 
-// cv::Mat createMarker(const cv::Ptr<cv::aruco::Dictionary> dict, const int id = 1, const int sidePixels = 400)
-// {
-//     cv::Mat marker;
-//     cv::aruco::drawMarker(dict, id, sidePixels, marker, 1);
-
-//     return marker;
-// }
-
-// // 创建aruco标记板
-// cv::Ptr<cv::aruco::GridBoard> createArucoBoard(cv::Mat &boardImg,
-//                                                const cv::Ptr<cv::aruco::Dictionary> &dict,
-//                                                const int markersX = 5,
-//                                                const int markersY = 7,
-//                                                const float markerLength = 0.04,
-//                                                const float markerSaparation = 0.01)
-// {
-//     auto board = cv::aruco::GridBoard::create(markersX, markersY, markerLength, markerSaparation, dict);
-//     board->draw(cv::Size(800, 600), boardImg, 10, 1);
-//     return board;
-// }
-
-// void markerDetect(const cv::Ptr<cv::aruco::Dictionary> dict,
-//                   cv::Mat &src,
-//                   std::vector<std::vector<cv::Point2f>> &corners,
-//                   std::vector<int> &ids)
-// {
-//     std::vector<std::vector<cv::Point2f>> rejectedImgPoints;
-//     auto parameters = cv::aruco::DetectorParameters::create();
-//     cv::aruco::detectMarkers(src, dict, corners, ids, parameters, rejectedImgPoints);
-//     cv::aruco::drawDetectedMarkers(src, corners, ids, cv::Scalar(0, 255, 0));
-
-//     std::cout << "corner size: " << corners.size() << std::endl;
-// }
-
-// // 增强标记检测
-// // aruco::refineDetectedMarkers()
-
 void fetchCameraParams(cv::Mat &cameraMatrix, cv::Mat &distCoeffs)
 {
     cameraMatrix = (cv::Mat_<double>(3, 3) << 674.6644165230119, 0, 322.2413306238805, 0, 673.8510341922967, 246.8952097749414, 0, 0, 1);
     distCoeffs = (cv::Mat_<double>(1, 5) << 0.07723134446474604, -0.05027556760465571, 0.003860032830530752, 0.001406508033458981, -0.5426930240877859);
 }
-
-// // marker位姿估计
-// bool boardPoseEstimation(cv::Mat &boardImg,
-//                          cv::Vec3d &rvecs,
-//                          cv::Vec3d &tvecs,
-//                          const cv::Ptr<cv::aruco::GridBoard> board,
-//                          const cv::Mat &cameraMatrix,
-//                          const cv::Mat &distCoeffs,
-//                          const cv::Ptr<cv::aruco::Dictionary> dict)
-// {
-//     std::vector<std::vector<cv::Point2f>> corners;
-//     std::vector<int> ids;
-//     markerDetect(dict, boardImg, corners, ids);
-//     std::cout << "---------------------------" << std::endl;
-
-//     int boardNum = cv::aruco::estimatePoseBoard(corners, ids, board, cameraMatrix, distCoeffs, rvecs, tvecs);
-//     std::cout << "boardNum: " << boardNum << std::endl;
-//     if (boardNum > 0)
-//     {
-//         cv::aruco::drawAxis(boardImg, cameraMatrix, distCoeffs, rvecs, tvecs, 0.04);
-//     }
-
-//     return true;
-// }
 
 Eigen::MatrixXd Mat2MatrixXd(const cv::Mat &R)
 {
@@ -113,7 +53,121 @@ void generateVirtualP3d(std::vector<cv::Point3f> &p3ds, const int nrows = 3, con
     }
 }
 
+void test_CharucoDiamond(cv::Mat &src_img,
+                         const cv::Mat &cameraMatrix,
+                         const cv::Mat &distCoeffs,
+                         const cv::Ptr<cv::aruco::Dictionary> &dict,
+                         const float squareLength = 0.038,
+                         const float markerLength = 0.019)
+{
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> markerCorners;
+
+    // detect aruco markers
+    cv::aruco::detectMarkers(src_img, dict, markerCorners, markerIds);
+    if (markerIds.empty() || markerCorners.empty())
+    {
+        return;
+    }
+
+    std::vector<cv::Vec4i> diamondIds;
+    std::vector<std::vector<cv::Point2f>> diamondCorners;
+
+    cv::aruco::detectCharucoDiamond(src_img, markerCorners, markerIds, squareLength / markerLength, diamondCorners, diamondIds);
+    if (diamondIds.empty() || diamondCorners.empty())
+    {
+        return;
+    }
+
+    std::vector<cv::Vec3d> rvecs, tvecs;
+    cv::aruco::estimatePoseSingleMarkers(diamondCorners, squareLength, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+    // draw axis
+    for (size_t i = 0; i < rvecs.size(); ++i)
+    {
+        cv::aruco::drawAxis(src_img, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.05);
+    }
+
+    // cv::imshow("inputImage", src_img);
+    // cv::waitKey(0);
+}
+
+void createCharucoDiamond(const cv::Ptr<cv::aruco::Dictionary> &dict, cv::Mat &output_marker)
+{
+    cv::aruco::drawCharucoDiamond(dict, cv::Vec4i(16, 45, 68, 28), 150, 75, output_marker);
+}
+
 int main(int argc, char **argv)
+{
+    auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME::DICT_6X6_250);
+    cv::Mat cameraMatrix, distCoeffs;
+    fetchCameraParams(cameraMatrix, distCoeffs);
+
+    cv::VideoWriter m_writer;
+    m_writer.open("demo.mp4" , CV_FOURCC('M', 'P', '4', '2') , 15 , cv::Size(640 , 480));
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened())
+    {
+        std::cerr << "open camera failture" << std::endl;
+        return 0;
+    }
+
+    cv::Mat frame;
+    while (true)
+    {
+        cap >> frame;
+        test_CharucoDiamond(frame, cameraMatrix, distCoeffs, dictionary);
+        m_writer.write(frame);
+        cv::imshow("frame", frame);
+        char key = cv::waitKey(25);
+
+        if (key == 'q')
+        {
+            break;
+        }
+    }
+    cap.release();
+    m_writer.release();
+
+    // float squareLength = 0.055;
+    // float markerLength = 0.027;
+
+    // float squareLength = 0.053;
+    // float markerLength = 0.026;
+
+    // float squareLength = 0.038;
+    // float markerLength = 0.019;
+
+    // std::vector<int> markerIds;
+    // std::vector<std::vector<cv::Point2f>> markerCorners;
+
+    // // detect aruco markers
+    // cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds);
+
+    // std::vector<cv::Vec4i> diamondIds;
+    // std::vector<std::vector<cv::Point2f>> diamondCorners;
+
+    // cv::aruco::detectCharucoDiamond(inputImage, markerCorners, markerIds, squareLength / markerLength, diamondCorners, diamondIds);
+
+    // std::vector<cv::Vec3d> rvecs, tvecs;
+    // cv::aruco::estimatePoseSingleMarkers(diamondCorners, squareLength, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+    // std::cout << "rvecs size: " << rvecs.size() << " , tvecs size: " << tvecs.size() << std::endl;
+    // std::cout << "rvecs: " << rvecs[0] << std::endl;
+    // std::cout << "tvecs: " << tvecs[0] << std::endl;
+    // // draw axis
+    // for (size_t i = 0; i < rvecs.size(); ++i)
+    // {
+    //     cv::aruco::drawAxis(inputImage, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.05);
+    // }
+
+    // cv::imshow("inputImage", inputImage);
+    // cv::waitKey(0);
+
+    return 1;
+}
+
+int main2(int argc, char **argv)
 {
     auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME::DICT_6X6_250);
 
@@ -138,24 +192,6 @@ int main(int argc, char **argv)
     // cv::cvtColor(back_img, back_img, CV_GRAY2BGR);
 
     cv::Mat src_img = cv::imread("/home/han/project/Opencv_project/cv_project/Aruco/c++/8.jpg");
-
-    // std::vector<cv::Vec3d> r_vecs;
-    // std::vector<cv::Vec3d> t_vecs;
-
-    // float scale = poseEstimation(src_img, r_vecs, t_vecs, cameraMatrix, distCoeffs, dictionary, 0.15, true);
-    // std::cout << "scale: " << scale << std::endl;
-    // cv::Mat rotation_matrix;
-    // cv::Rodrigues(r_vecs[0], rotation_matrix);
-    // std::cout << "rotate matrix: " << rotation_matrix << std::endl;
-    // std::cout << "translation: " << t_vecs[0] << std::endl;
-
-    // Eigen::Matrix3d eigen_rotaion_matrix = Mat2MatrixXd(rotation_matrix);
-    // std::cout << eigen_rotaion_matrix << std::endl;
-
-    // //2.1 旋转矩阵转换为欧拉角
-    // //ZYX顺序，即先绕x轴roll,再绕y轴pitch,最后绕z轴yaw,0表示X轴,1表示Y轴,2表示Z轴
-    // Eigen::Vector3d euler_angles = eigen_rotaion_matrix.eulerAngles(2, 1, 0);
-    // std::cout << "yaw(z) pitch(y) roll(x) = " << euler_angles.transpose() << std::endl;
 
     std::shared_ptr<PoseEstamation> poseEstimation_obj = std::make_shared<PoseEstamation>(dictionary);
     float scale = poseEstimation_obj->poseEstimation(src_img, true);
@@ -226,7 +262,7 @@ int main(int argc, char **argv)
     const int nrows = 3;
     const int ncols = 3;
 
-    generateVirtualP3d(generate_p3d , nrows ,  ncols , 0.075);
+    generateVirtualP3d(generate_p3d, nrows, ncols, 0.075);
 
     cv::projectPoints(generate_p3d, rvec, tvec, cameraMatrix, distCoeffs, projectedPoints_);
     std::cout << "size: " << generate_p3d.size() << std::endl;
@@ -262,7 +298,6 @@ int main(int argc, char **argv)
     }
 
     cv::imshow("src_img", src_img);
-    // cv::imshow("dst", dst);
     cv::waitKey(0);
 
     return 0;
